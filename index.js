f3be4a38ba5bd29941f69676146ac361731c614f
Christine Wilks <crissxross@gmail.com>
2021-10-05 14:58:13 +0100

    First version

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+console.log(`
+Christine Wilks, PhD
+
+Digital writer, artist, developer and practice-based researcher. I create interactive narratives and creative web apps.
+
+Site:    https://crissxross.net
+Twitter: https://twitter.com/crissxross
+GitHub:  https://github.com/crissxross
+
+`);
