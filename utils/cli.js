bdf4bd9aae46dba4c8d7d0f8e2266cc401625e7f
Christine Wilks <crissxross@gmail.com>
2021-10-10 15:19:16 +0100

    adds -v  version info shortcut

--- utils/cli.js
+++ utils/cli.js
@@ -2,14 +2,18 @@ import meow from 'meow';
 
 const helpText = `
   Usage
-    npx christine-creates [options]
+    npx christine-creates [options] <commands >
 
   Options
-    social       Show the social info
-    --no-social  Don't show the social info
-    ad           Show the ad info
-    --no-ad      Don't show the ad info
-    -d, --debug  Print debugging info
+    social        Print the social info
+    --no-social   Don't print the social info
+    ad            Print the ad info
+    --no-ad       Don't print the ad info
+    -d, --debug   Print debugging info
+    -v, --version Print CLI version
+
+  Commands
+    help          Print CLI help info
 
   Examples
   npx christine-creates --no-social
@@ -30,6 +34,11 @@ const options = {
       type: 'boolean',
       default: false,
       alias: 'd'
+    },
+    version: {
+      type: 'boolean',
+      default: false,
+      alias: 'v'
     },
   }
 };
