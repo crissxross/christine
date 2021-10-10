6721684d7cd787b81781b2c17f222f37a2c13641
Christine Wilks <crissxross@gmail.com>
2021-10-10 14:32:02 +0100

    adds flags options & helpText

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 #!/usr/bin/env node
 
+import meow from 'meow';
 import logSymbols from 'log-symbols';
 import cliAlerts from 'cli-alerts';
 import cliHandleError from 'cli-handle-error';
 
 import { init } from './utils/init.js';
-import * as data from './utils/data.js';
+import { name, bio, social, ad } from './utils/data.js';
+
+const helpText = `
+  Usage
+    npx christine-creates [options]
+
+  Options
+    social       Show the social info
+    --no-social  Don't show the social info
+    ad           Show the ad info
+    --no-ad      Don't show the ad info
+
+  Examples
+  npx christine-creates --no-social
+
+`;
+const options = {
+  importMeta: import.meta,
+  flags: {
+    social: {
+      type: 'boolean',
+      default: true,
+    },
+    ad: {
+      type: 'boolean',
+      default: true,
+    },
+  }
+};
+const cli = meow(helpText, options);
+const input = cli.input;
+const flags = cli.flags;
 
 (async () => {
   init();
-  console.log(data.name);
-  console.log(data.bio);
-  console.log(data.social);
-  cliAlerts({type: `info`, msg: data.ad});
+  console.log(name);
+  console.log(bio);
+
+  if (flags.social) {
+    console.log(social);
+  }
+
+  if (flags.ad) {
+    cliAlerts({type: `info`, msg: ad});
+  }
+
+  cliAlerts({type: `info`, msg: `CLI DATA ↓`});
+  console.log(`input`, input);
+  console.log(`flags`, flags);
+  console.log();
 })();
 
