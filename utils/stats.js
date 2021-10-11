87567b8b852da32af0aae2b3fbda46c9260fb3fb
Christine Wilks <crissxross@gmail.com>
2021-10-11 16:13:32 +0100

    adds loading spinner & messages

--- utils/stats.js
+++ utils/stats.js
@@ -1,15 +1,20 @@
+import ora from "ora";
+import chalk from "chalk";
 import axios from "axios";
 import cliHandleError from 'cli-handle-error';
 import { to } from 'await-to-js';
 
+const spinner = ora({text: ''});
 const apiURL = `https://api.github.com/users/crissxross`;
 
 const stats = async () => {
-
+  spinner.start(`${chalk.yellow(`FOLLOWERS`)} fetching...`);
   const [error, res] = await to(axios.get(apiURL));
   cliHandleError(`API CALL FAILED`, error, true, true);
 
   const ghFollowers = res.data.followers;
+  spinner.succeed(`${chalk.green(`FOLLOWERS`)} fetched`);
+  // spinner.stop();
 
   console.log(`
     Github followers: ${ghFollowers}
