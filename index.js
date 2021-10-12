b4f8ce62ebc1ad28e138ed7269dbce798748249b
Christine Wilks <crissxross@gmail.com>
2021-10-12 11:29:14 +0100

    adds enquirer prompt input/output

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 import logSymbols from 'log-symbols';
 import boxen from 'boxen';
 import cliAlerts from 'cli-alerts';
-import cliHandleError from 'cli-handle-error';
+import enquirerPkg from 'enquirer';
+// because it's a CommonJS package
+const { prompt, Confirm, Toggle } = enquirerPkg;
 
 import { init } from './utils/init.js';
 import { name, bio, social, ad, blog, site } from './utils/data.js';
@@ -11,7 +13,6 @@ import { cli } from './utils/cli.js';
 import { debug } from './utils/debug.js';
 import { stats } from './utils/stats.js';
 import { posts } from './utils/posts.js';
-
 
 const input = cli.input;
 const flags = cli.flags;
@@ -21,6 +22,13 @@ const flags = cli.flags;
   init(flags.minimal, flags.clear);
   input.includes('help') && cli.showHelp(0);
 
+  const prompt = new Toggle({
+    message: 'Before proceeding, please respond:'
+  });
+  const response = await prompt.run();
+
+  console.log('response', response);
+
   // Print out the info
   flags.ad && cliAlerts({type: `info`, msg: ad});
   flags.bio &&
@@ -29,6 +37,18 @@ const flags = cli.flags;
   flags.social && console.log(social);
   flags.posts && cliAlerts({type: `info`, msg: blog, name: site});
   flags.posts && (await posts());
+
+  // stats
+  await stats();
+
+  // debug info if needed
+  debug(flags.debug, cli);
+
+})();
+
+// NOTES:
+
+// ALTERNATIVE way of expressing 'Print out the info' above
 
   // if (flags.bio) {
   //   console.log(name);
@@ -43,11 +63,19 @@ const flags = cli.flags;
   //   cliAlerts({type: `info`, msg: ad});
   // }
 
-  // stats
-  await stats();
+// ALTERNATIVE enquirer prompts
 
-  // debug info if needed
-  debug(flags.debug, cli);
+// const questions = [
+//   {
+//     type: 'input',
+//     name: 'name',
+//     message: 'What is your name?',
+//   },
+//   {
+//     type: 'input',
+//     name: 'username',
+//     message: 'What is your username?',
+//   }
+// ];
 
-})();
-
+// const response = await prompt(questions);
